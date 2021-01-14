5aa88d60eb3efd769e10da0a3af7b5b903f249e1
Alex Bordo <b4zx720@gmail.com>
2021-01-14 14:49:24 +0300

    fix main-routes.js

--- src/express/routes/main-routes.js
+++ src/express/routes/main-routes.js
@@ -4,7 +4,6 @@ const {Router} = require(`express`);
 const mainRouter = new Router();
 
 mainRouter.get(`/`, (req, res) => res.render(`main`));
-mainRouter.get(`/register`, (req, res) => res.render(`sign-up`));
 mainRouter.get(`/login`, (req, res) => res.render(`login`));
 mainRouter.get(`/register`, (req, res) => res.render(`sign-up`));
 mainRouter.get(`/search`, (req, res) => res.render(`search`));
