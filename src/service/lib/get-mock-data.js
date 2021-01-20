4c0578a06752bf06977e188a51d8e9cb642dd88e
Alex Bordo <b4zx720@gmail.com>
2021-01-20 16:07:47 +0300

    getMockData refactor

--- src/service/lib/get-mock-data.js
+++ src/service/lib/get-mock-data.js
@@ -3,21 +3,15 @@
 const fs = require(`fs`).promises;
 const {FILE_NAME} = require(`../../const`);
 
-let data = [];
-
 const getMockData = async () => {
+  let data = [];
 
   if (data.length > 0) {
     return data;
   }
 
-  try {
-    const fileContent = await fs.readFile(FILE_NAME);
-    data = JSON.parse(fileContent);
-
-  } catch (err) {
-    throw err;
-  }
+  const fileContent = await fs.readFile(FILE_NAME);
+  data = JSON.parse(fileContent);
 
   return data;
 };
